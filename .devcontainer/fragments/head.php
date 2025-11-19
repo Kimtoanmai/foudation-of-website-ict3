96c23ac0d7d55991af0fbb57a056bd1e416366e1
Kimtoanmai <toancamxuc2707@gmail.com>
2025-11-19 08:42:52 +0000

    ve sua lai duma

---
+++ .devcontainer/fragments/head.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="w=en">
+
+
+<header class="main-header">
+        <button type="button" class="nav-button">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <a href="/">
+            <img class="logo" src="logoT .jpg" alt="oi doi oi logo">
+        </a>
+        <form class="search" method="get" action="/search.php">
+            <input type="search" placeholder="search for a product">
+            <button>
+                <img src="https://via.placeholder.com/20" alt="search icon">
+
+            </button>
+        </form>
+        <nav class="user-nav">
+            <a href="stores.html">
+                <span class="material-symbols-outlined">
+                    storefront
+                </span>
+            </a>
+            <a href="account.html">
+                <span class="material-symbols-outlined">
+                    person
+                </span>
+            </a>
+            <a href="cart.html">
+                <span class="material-symbols-outlined">
+                    shopping_cart
+                </span>
+            </a>
+        </nav>
+        <nav class="shop-nav">
+            <a href="womens.html">Womens</a>
+            <a href="mens.html">Mens</a>
+            <a href="kids.html">Kids</a>
+            <a href="brands.html">Brands</a>
+            <a href="clearance.html">Clearance</a>
+        </nav>
+    </header>
---
+++ .devcontainer/fragments/pageheader.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang= "en">
+
+<?php include "fragments/head.php"; ?>
+
+<body>
+    <?php include "fragments/pageheader.php"; ?>
+    <!-- carousel -->
+    <div class="main-carousel" data-flickity='{ "autoPlay": true, "cellAlign": "left", "contain": true }'>
+        <div class="slide">
+            <h2>Black Friday Sale</h2>
+            <a href="#" class="slide-button">Shop Black Friday</a>
+        </div>
+        <div class="slide">i want to buy a playstation 5 </div>
+        <div class="slide">can someone buy for me pls ?</div>
+        <div class="slide">i think i have to hard work to buy it </div>
+    </div>
+    <main class="content">
+        <div class="card">
+            <img src="womensp.jpg" alt="a woman sit in meditateion pose">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="mansp.jpg" alt="a man tying his shoes">
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="kidssp.jpg" alt="a child running in a baseball game">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+        <div class="banner">
+            <h2>Black Friday</h2>
+            <a class="slide-button" href="#">Shop Sale</a>
+        </div>
+        <div class="card">
+            <img src="woman2.jpg" alt="a woman doing push ups">
+            <div class="card-content">
+                <h3>Womens</h3>
+                <a href="#" class="card-button">Shop Womens</a>
+            </div>
+        </div>
+         <div class="card">
+            <img src="man2 (1).jpg" alt="a man playing football">
+
+            <div class="card-content">
+                <h3>Mens</h3>
+                <a href="#" class="card-button">Shop Mens</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="kids2.jpg" alt="child playing football">
+            <div class="card-content">
+                <h3>Kids</h3>
+                <a href="#" class="card-button">Shop Kids</a>
+            </div>
+        </div>
+    </main>
+</body>
